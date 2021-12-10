de98532e7584fb48c716753423478297cddb72cb
Heiko Goretzki <heikogoretzki@gmail.com>
2021-12-10 18:01:29 +0100

    initial

---
+++ src/main/java/de/mischokacademy/NetPlanDesignerUI/NetPlanDesignerUiApplication.java
@@ -0,0 +1,13 @@
+package de.mischokacademy.NetPlanDesignerUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetPlanDesignerUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetPlanDesignerUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mischokacademy/NetPlanDesignerUI/NetPlanDesignerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mischokacademy.NetPlanDesignerUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetPlanDesignerUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
