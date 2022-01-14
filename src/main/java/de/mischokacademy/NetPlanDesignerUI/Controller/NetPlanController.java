0da39ed30ad4c8bc1b7dbaa5411ba9494a6f40f4
heikogoretzki <heikogoretzki@gmail.com>
2022-01-14 11:56:43 +0100

    add database

--- src/main/java/de/mischokacademy/NetPlanDesignerUI/Controller/NetPlanController.java
+++ src/main/java/de/mischokacademy/NetPlanDesignerUI/Controller/NetPlanController.java
@@ -2,6 +2,9 @@ package de.mischokacademy.NetPlanDesignerUI.Controller;
 
 import de.mischokacademy.NetPlanDesignerUI.Domain.Knot;
 import de.mischokacademy.NetPlanDesignerUI.Domain.KnotInputForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,8 +20,22 @@ import java.util.Objects;
 public class NetPlanController {
     private static final List<KnotInputForm> knotInputFormList = new ArrayList<>();
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @GetMapping("/")
     public String getStartPage() {
+
+        String filterName = "Julius";
+        String queryTemplate = "SELECT * FROM test WHERE name = '%s';";
+        String query = String.format(queryTemplate, filterName);
+        System.out.println(query);
+        jdbcTemplate.execute(query);
+
+        SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet("SELECT * FROM test");
+        while (sqlRowSet.next()) {
+            System.out.println(sqlRowSet.getInt("id") + " -> " + sqlRowSet.getString("name"));
+        }
 
         return "home";
     }
